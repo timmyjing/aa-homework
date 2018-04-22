89fa31e8ffe99707e312ddb3f0c128ce46e7ff50
Timmy Jing <timmyjing@gmail.com>
2018-04-21 21:16:00 -0700

    Implemented functions to make Simon playable.

--- W2D1/skeleton/lib/simon.rb
+++ W2D1/skeleton/lib/simon.rb
@@ -7,30 +7,39 @@ class Simon
     @seq = []
     @sequence_length = 1
     @game_over = false
+    play
   end
 
   def play
     take_turn until game_over
     game_over_message
     reset_game
+    play
   end
 
   def take_turn
-    unless game_over
+      system('clear')
       show_sequence
       require_sequence
-      round_success_message
-      @sequence_length += 1
-    end
-
+      unless game_over
+        round_success_message
+        @sequence_length += 1
+      end
   end
 
   def show_sequence
     add_random_color
+    seq.each_with_index do |color, idx|
+      print color
+      sleep(0.75)
+      system('clear')
+    end
   end
 
   def require_sequence
-
+    puts "Enter the color combination (ex: red blue green yellow): "
+    colors = gets.chomp.downcase.split(" ").map(&:to_s)
+    @game_over = true unless colors == seq
   end
 
   def add_random_color
@@ -38,11 +47,13 @@ class Simon
   end
 
   def round_success_message
-
+    puts "Success! Current sequence length: #{sequence_length}"
+    sleep(1.5)
   end
 
   def game_over_message
-
+    puts "Incorrect sequence. Starting over."
+    sleep(1.5)
   end
 
   def reset_game
@@ -51,3 +62,5 @@ class Simon
     @seq = []
   end
 end
+
+game = Simon.new
